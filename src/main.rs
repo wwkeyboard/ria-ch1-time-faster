3d6e41091ae7953e6fba89bf533731eafc4a6a56
Aaron Lee <alee@aaronosaur.us>
2020-04-19 12:28:27 -0500

    working actix-web hello-world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body(
+        "<h1>Hello, Rust in Action!</h1>
+  <p>What is the <a href=\"/now\">time</a>?</p>",
+    )
+}
+
+#[get("/now")]
+async fn now() -> impl Responder {}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
