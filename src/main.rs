1b9f9ac69d81241f3074cdae9059fef73820a05d
Aaron Lee <alee@aaronosaur.us>
2020-04-19 12:35:07 -0500

    use serde for json and return the time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use chrono::prelude::*;
+use serde::Serialize;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -8,12 +10,23 @@ async fn index() -> impl Responder {
     )
 }
 
+#[derive(Serialize)]
+struct Timestamp {
+    t: String,
+}
+
 #[get("/now")]
-async fn now() -> impl Responder {}
+async fn now() -> impl Responder {
+    let now: DateTime<Utc> = Utc::now();
+    let timestamp = Timestamp {
+        t: now.to_rfc3339(),
+    };
+    HttpResponse::Ok().json(timestamp)
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(now))
         .bind("127.0.0.1:8080")?
         .run()
         .await
